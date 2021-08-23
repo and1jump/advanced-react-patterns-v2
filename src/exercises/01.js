1c95485fa5c733fa4600d38522ca76757cf69d88
Daniil <onejump9@gmail.com>
2021-08-23 16:52:02 +0300

    added rep, made toggle component exercise, compound component

---
+++ src/exercises/01.js
@@ -0,0 +1,45 @@
+// Building the toggle component
+
+import React from 'react'
+// 🐨 uncomment this import to get the switch component.
+// It takes an `onClick` and an `on` prop
+import {Switch} from '../switch'
+
+class Toggle extends React.Component {
+  // 🐨 this toggle component is going to need to have state for `on`
+  //
+  // You'll also want a method to handle when the switch is clicked
+  // which will update the `on` state and call the `onToggle` prop
+  // 💰 this.props.onToggle(this.state.on)
+  state = {
+    on: false,
+  }
+
+  toggle = () => {
+    this.setState(
+      (prevState) => ({
+        on: !prevState.on,
+      }),
+      () => {
+        this.props.onToggle(this.state.on)
+      },
+    )
+  }
+
+  render() {
+    // 🐨 here you'll want to return the switch with the `on` and `onClick` props
+    return <Switch on={this.state.on} onClick={this.toggle} />
+  }
+}
+
+// Don't make changes to the Usage component. It's here to show you how your
+// component is intended to be used and is used in the tests.
+// You can make all the tests pass by updating the Toggle component.
+function Usage({
+  onToggle = (...args) => console.log('onToggle', ...args),
+}) {
+  return <Toggle onToggle={onToggle} />
+}
+Usage.title = 'Build Toggle'
+
+export {Toggle, Usage as default}
---
+++ src/exercises/03.js
@@ -0,0 +1,98 @@
+// Flexible Compound Components with context
+
+import React from 'react'
+import {Switch} from '../switch'
+
+// Right now our component can only clone and pass props to immediate children.
+// So we need some way for our compound components to implicitly accept the on
+// state and toggle method regardless of where they're rendered within the
+// Toggle component's "posterity" :)
+//
+// The way we do this is through context. React.createContext is the API we
+// want. Here's a simple example of that API:
+//
+// const defaultValue = 'light'
+// const ThemeContext = React.createContext(defaultValue)
+//
+// ...
+// <ThemeContext.Provider value={this.state}>
+//   {this.props.children}
+// </ThemeContext.Provider>
+// ...
+//
+// ...
+// <ThemeContext.Consumer>
+//   {value => <div>The current theme is: {value}</div>}
+// </ThemeContext.Consumer>
+// ...
+
+// 🐨 create a ToggleContext with React.createContext here
+
+const ToggleContext = React.createContext({
+  on: false,
+  toggle: () => {},
+})
+
+class Toggle extends React.Component {
+  // 🐨 each of these compound components will need to be changed to use
+  // ToggleContext.Consumer and rather than getting `on` and `toggle`
+  // from props, it'll get it from the ToggleContext.Consumer value.
+  static On = ({children}) => (
+    <ToggleContext.Consumer>
+      {(contextValue) => (contextValue.on ? children : null)}
+    </ToggleContext.Consumer>
+  )
+  static Off = ({children}) => (
+    <ToggleContext.Consumer>
+      {(contextValue) => (contextValue.on ? null : children)}
+    </ToggleContext.Consumer>
+  )
+  static Button = ({on, toggle, ...props}) => (
+    <ToggleContext.Consumer>
+      {(contextValue) => (
+        <Switch
+          on={contextValue.on}
+          onClick={contextValue.toggle}
+          {...props}
+        />
+      )}
+    </ToggleContext.Consumer>
+  )
+  // Because we'll be passing state into context, we need to 🐨 add the
+  // toggle function to state.
+  // 💰 You'll need to move this below the `toggle` function. See
+  // if you can figure out why :)
+  toggle = () =>
+    this.setState(
+      ({on}) => ({on: !on}),
+      () => this.props.onToggle(this.state.on),
+    )
+  state = {on: false, toggle: this.toggle}
+  render() {
+    return (
+      <ToggleContext.Provider value={this.state}>
+        {this.props.children}
+      </ToggleContext.Provider>
+    )
+  }
+}
+
+// Don't make changes to the Usage component. It's here to show you how your
+// component is intended to be used and is used in the tests.
+// You can make all the tests pass by updating the Toggle component.
+function Usage({
+  onToggle = (...args) => console.log('onToggle', ...args),
+}) {
+  return (
+    <Toggle onToggle={onToggle}>
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Off>The button is off</Toggle.Off>
+      <div>
+        <Toggle.Button />
+      </div>
+    </Toggle>
+  )
+}
+Usage.title = 'Flexible Compound Components'
+
+export {Toggle, Usage as default}
